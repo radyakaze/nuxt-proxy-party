124514000e8c5049e51ee004fe4336a1814873dd
Pringgo Radianto <pringgo.radianto@privyid.tech>
2024-10-01 13:57:52 +0700

    chore(logger): refactor logger to use consola utils instead of chalk

--- src/runtime/utils/logger.ts
+++ src/runtime/utils/logger.ts
@@ -1,13 +1,13 @@
 import { createConsola } from 'consola'
-import chalk, { type Chalk } from 'chalk'
+import { type colors, colorize } from 'consola/utils'
 
-const logStyles: Record<string, { symbol: string, color: Chalk }> = {
-  log: { symbol: '\u25A1', color: chalk.white },
-  info: { symbol: '\u2139', color: chalk.blue },
-  success: { symbol: '\u2714', color: chalk.green },
-  warn: { symbol: '\u26A0', color: chalk.yellow },
-  error: { symbol: '\u2716', color: chalk.red },
-  start: { symbol: '\u25B6', color: chalk.magenta },
+const logStyles: Record<string, { symbol: string, color: keyof typeof colors }> = {
+  log: { symbol: '\u25A1', color: 'white' },
+  info: { symbol: '\u2139', color: 'blue' },
+  success: { symbol: '\u2714', color: 'green' },
+  warn: { symbol: '\u26A0', color: 'yellow' },
+  error: { symbol: '\u2716', color: 'red' },
+  start: { symbol: '\u25B6', color: 'magenta' },
 }
 
 export default createConsola({
@@ -15,7 +15,7 @@ export default createConsola({
     {
       log(logObj) {
         const style = logStyles[logObj.type] || logStyles.log
-        console.log(`${style.color(style.symbol)} ${style.color('[Nuxt Proxy Party]')}:`, logObj.args.join(' '))
+        console.log(`${colorize(style.color, style.symbol)} ${colorize(style.color, '[Nuxt Proxy Party]')}:`, logObj.args.join(' '))
       },
     },
   ],
